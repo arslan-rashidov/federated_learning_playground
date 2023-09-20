cb0e99f292c17e85d6ea60dc5ab87bbb3cfb706d
arslan-rashidov <mfcitycapital@mail.ru>
2023-09-20 14:25:40 +0300

    fixed example

--- anti_fraud_fl/client_methods.py
+++ anti_fraud_fl/client_methods.py
@@ -83,7 +83,7 @@ def train(model: torch.nn.Module, train_set: torch.utils.data.Dataset, epochs: i
         train_epoch_loss /= len(train_dataloader)
         train_epoch_loss_metric.log_value(train_epoch_loss)
 
-    return (train_epoch_loss_metric, model)
+    return ([train_epoch_loss_metric], model)
 
 
 def test(model: torch.nn.Module, test_set: torch.utils.data.Dataset, return_output: bool) -> Union[
